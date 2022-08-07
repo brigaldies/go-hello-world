7a89a77ef8c20a7a793921282450a01c08062606
Bertrand Rigaldies <brigaldies@gmail.com>
2022-08-07 09:14:25 -0400

    Added the morestrings package in the module.

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 // Standard packages do not have import prefixes
-import "fmt"
+import (
+	"fmt"
+	"github.com/brigaldies/go-hello-world/morestrings"
+)
 
 func main() {
-    fmt.Println("Hello, Go world.")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 }
-
